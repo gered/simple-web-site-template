c8661f091136eed94a3a2a7bfb79ff4e19f9ad80
gered <gered@blarg.ca>
2022-01-07 21:00:47 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.gered/lein-template.simple-web-site "0.1.0-SNAPSHOT"
+(defproject net.gered/lein-template.simple-web-site "0.1.0"
   :description         "Simple Clojure web site project template."
   :url                 "https://github.com/gered/simple-web-site-template"
   :license             {:name "MIT License"
